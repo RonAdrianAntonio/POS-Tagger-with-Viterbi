2673c81aaf07db116b7a6f8835ba786baa227784
RonAdrianAntonio <ronadrian.antonio@gmail.com>
2021-09-01 16:02:51 -0500

    Made symbol array wherein the first element is NaN, and all subsequent elements are the possible symbols, in order.

--- viterbi.py
+++ viterbi.py
@@ -1,5 +1,4 @@
 import sys
-NUMBER_OF_OBVS = 5
 #below is just number of symbol emitted-1, since this is
 #how it's stored in an array
 OBVS = [0, 3, 1, 1, 3]
@@ -29,32 +28,42 @@ def main(transName, emiName, startProbName, posTagsName):
     #for x in startArr:
     #    print(x)
     with open(posTagsName, 'r') as posFile:
-        posArr = [x for x in posFile.read().split()]
+        posArr = ['NaN'] + [x for x in posFile.read().split()]
 
 
-    viterbiArr = [[0.0 for symbol in range(len(transArr))] for state in range(NUMBER_OF_OBVS)]
-    viterbiMaxPrevState = [['' for symbol in range(len(transArr))] for state in range(NUMBER_OF_OBVS)]
+    viterbiArr = [[0.0 for symbol in range(len(transArr))] for state in range(len(OBVS))]
+    viterbiMaxPrevState = [[-1 for symbol in range(len(transArr))] for state in range(len(OBVS))]
 
     for currIdx in range(len(viterbiArr)):
         if currIdx < 1:
             for currStateIdx in range(len(emiArr)):
                 viterbiArr[currIdx][currStateIdx] = emiArr[currStateIdx][OBVS[currIdx]] * startArr[currStateIdx]
-                viterbiMaxPrevState[currIdx][currStateIdx] = 'NaN'
+                viterbiMaxPrevState[currIdx][currStateIdx] = 0
             #print(viterbiArr[0])
         else:
             for currStateIdx in range(len(emiArr)):
                 maxVal, maxIdx = getMaxState(viterbiArr[currIdx-1], transArr, currStateIdx)
                 viterbiArr[currIdx][currStateIdx] = emiArr[currStateIdx][OBVS[currIdx]] * maxVal
-                viterbiMaxPrevState[currIdx][currStateIdx] = posArr[maxIdx]#'y'+str(maxIdx+1)
+                viterbiMaxPrevState[currIdx][currStateIdx] = maxIdx + 1#'y'+str(maxIdx+1)
 
+    print("Table:")
     for x in viterbiArr:
         print(x)
-                
-
+    
+    
+    print("\nSymbols:")
     for x in viterbiMaxPrevState:
-        print(x)
+        print([posArr[y] for y in x])
 
+    finalList = []
+    for currPos in range(len(OBVS)-1, -1, -1):
+        maxIdx = max(viterbiArr[currPos])
+        print(maxIdx)
                 
+
+    print("\nFinal Result:")
+    print(finalList)
+
 
 
 if __name__ == '__main__':
