68aff3107ad4fe4d5b64c97e33c8d49a9d5bfc0b
RonAdrianAntonio <ronadrian.antonio@gmail.com>
2021-09-02 16:34:41 -0500

    added sample run script to give an example to the user on how to run the program with python 3

---
+++ samplerun.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# this is just an example of a command that can be used to run my Viterbi implementation
+
+python3 main.py sampleHMMs/DNA/transitions.txt sampleHMMs/DNA/emissions.txt sampleHMMs/DNA/startprob.txt sampleHMMs/DNA/states.txt sampleHMMs/DNA/symbols.txt
