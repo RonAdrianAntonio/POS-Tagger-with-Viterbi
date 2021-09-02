20c2ac3ec8074a77c1de30da5aee3f75a506cc43
RonAdrianAntonio <ronadrian.antonio@gmail.com>
2021-09-02 16:07:08 -0500

    Added a simple shell for main.py. This is so that we don't have to keep running the program everytime we want to run Viterbi algorithm on the same HMM but with a different symbol sequence

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ from viterbi.viterbi import viterbi as vt
 
 
 def main(transName, emiName, startProbName, posTagsName, symbolsName):
-    print("Please enter the symbol sequence")
-    observation = input()
-
     viter = vt(transName, emiName, startProbName, posTagsName, symbolsName)
 
-    viter.run(observation)
+    print("Welcome to my Viterbi implementation!")
+    while True:
+        print("\n\nPlease type in 'exit' for the symbol sequence to exit out of the program.")
+        print("Please enter the symbol sequence")
+        observation = input()
+        if observation == "exit":
+            print("Thank you for using my Viterbi implementation!")
+            return
+        viter.run(observation)
 
 
 if __name__ == '__main__':
