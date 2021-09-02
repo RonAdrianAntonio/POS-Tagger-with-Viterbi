1921c7f84e7a61dc46c2a30ac043eb9774a9529b
RonAdrianAntonio <ronadrian.antonio@gmail.com>
2021-09-01 22:11:21 -0500

    Added string input

--- viterbi.py
+++ viterbi.py
@@ -1,7 +1,8 @@
 import sys
 #below is just number of symbol emitted-1, since this is
 #how it's stored in an array
-OBVS = [0, 3, 1, 1, 3]
+#observation = [0, 3, 1, 1, 3]
+#observation = [2, 2, 1, 0, 1, 3, 2, 0, 0]
 def getMaxState(row, transitions, currentState):
     maxVal = float('-inf')
     currMaxIdx = -1;
@@ -13,37 +14,36 @@ def getMaxState(row, transitions, currentState):
     return maxVal, currMaxIdx
 
 def main(transName, emiName, startProbName, posTagsName):
+    print("Please enter the symbol sequence")
+    observation = input()
+
+
     with open(transName, 'r') as transFile:
         transArr = [[float(x) for x in line.split()] for line in transFile.read().split('\n') if len(line) > 0]
-    #for x in transArr:
-    #    print(x)
 
     with open(emiName, 'r') as emiFile:
         emiArr = [[float(x) for x in line.split()] for line in emiFile.read().split('\n') if len(line) > 0]
-    #for x in emiArr:
-    #    print(x)
     
     with open(startProbName, 'r') as strtFile:
         startArr = [float(x) for x in strtFile.read().split()]
-    #for x in startArr:
-    #    print(x)
+
     with open(posTagsName, 'r') as posFile:
         posArr = ['NaN'] + [x for x in posFile.read().split()]
 
 
-    viterbiArr = [[0.0 for symbol in range(len(transArr))] for state in range(len(OBVS))]
-    viterbiMaxPrevState = [[-1 for symbol in range(len(transArr))] for state in range(len(OBVS))]
+    viterbiArr = [[0.0 for symbol in range(len(transArr))] for state in range(len(observation))]
+    viterbiMaxPrevState = [[-1 for symbol in range(len(transArr))] for state in range(len(observation))]
 
     for currIdx in range(len(viterbiArr)):
         if currIdx < 1:
             for currStateIdx in range(len(emiArr)):
-                viterbiArr[currIdx][currStateIdx] = emiArr[currStateIdx][OBVS[currIdx]] * startArr[currStateIdx]
+                viterbiArr[currIdx][currStateIdx] = emiArr[currStateIdx][observation[currIdx]] * startArr[currStateIdx]
                 viterbiMaxPrevState[currIdx][currStateIdx] = 0
             #print(viterbiArr[0])
         else:
             for currStateIdx in range(len(emiArr)):
                 maxVal, maxIdx = getMaxState(viterbiArr[currIdx-1], transArr, currStateIdx)
-                viterbiArr[currIdx][currStateIdx] = emiArr[currStateIdx][OBVS[currIdx]] * maxVal
+                viterbiArr[currIdx][currStateIdx] = emiArr[currStateIdx][observation[currIdx]] * maxVal
                 viterbiMaxPrevState[currIdx][currStateIdx] = maxIdx + 1#'y'+str(maxIdx+1)
 
     print("Table:")
@@ -63,7 +63,7 @@ def main(transName, emiName, startProbName, posTagsName):
     finalStateSigma = viterbiArr[len(viterbiMaxPrevState)-1]
     finalStateIdx = finalStateSigma.index(max(finalStateSigma))+1
     
-    for currPos in range(len(OBVS)-1, -1, -1):
+    for currPos in range(len(observation)-1, -1, -1):
         finalList.append(posArr[finalStateIdx])
         finalStateIdx = viterbiMaxPrevState[currPos][finalStateIdx -1]
 
